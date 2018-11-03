7aabfe53082093bed388536c6cedf77d9ee5ab05
guard2205 <gctiberio@gmail.com>
2018-11-03 18:24:34 +0100

    ASP exo
    asp

---
+++ ChoixRestoAvecTests/Models/BddContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace ChoixRestoAvecTests.Models
+{
+    public class BddContext : DbContext
+    {
+        public DbSet<Sondage> Sondages { get; set; }
+        public DbSet<Resto> Restos { get; set; }
+        public DbSet<Utilisateur> Utilisateurs { get; set; }
+    }
+    
+}
---
+++ ChoixRestoAvecTests/Models/Resto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ChoixRestoAvecTests.Models
+{
+    [Table("Restos")]
+    public class Resto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nom { get; set; }
+        public string Telephone { get; set; }
+    }
+}
